71ed4f015c71f13ced3421d7f247a0b9d602adb5
ProfEibe <523386+profeibe@users.noreply.github.com>
2023-10-06 19:36:39 +0200

    display standings

--- src/app/league/league.component.ts
+++ src/app/league/league.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { LeagueService } from './league.service';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-league',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './league.component.html',
   styleUrls: ['./league.component.css'],
 })
@@ -14,14 +15,16 @@ export class LeagueComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private service = inject(LeagueService);
 
+  standings = [];
+
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (!id) return;
       console.log(params.get('id'));
-      this.service
-        .getStanding(id)
-        .subscribe((standings) => console.log(standings));
+      this.service.getStanding(id).subscribe((standings) => {
+        this.standings = standings.response[0].league.standings[0];
+      });
     });
   }
 }
